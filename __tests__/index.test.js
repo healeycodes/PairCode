4b7f3d4d44594d9337d9718245bc3bbd8ca14d8c
Andrew <healeycodes@gmail.com>
2019-05-04 11:49:17 +0100

    tests: semi-colons and slight refactor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,7 @@
  */
 describe('models/index', () => {
     it('Returns the Room model', () => {
-        const models = require('../models')
-        expect(models.Room).not.toBeUndefined()
-    })
-})
+        const models = require('../models');
+        expect(models.Room).not.toBeUndefined();
+    });
+});
